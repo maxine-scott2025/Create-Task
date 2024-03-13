35c80d7dae33c789852e074f5d17b2e18048f638
maxine-scott2025 Scott <flojo734@gmail.com>
2024-03-13 21:11:54 +0000

    first variables

---
+++ CreateTask.py
@@ -0,0 +1,6 @@
+compliment = {"Red": "Green", "Blue": "Orange", "Yellow": "Violet"}
+
+trio = {"Orange": ["red-orange", "orange", "yellow-orange"], "Green": ["yellow-green", "green", "blue-green"], "Violet": ["blue-violet", "violet", "red-violet"]}
+
+base_color = input("Please input a color to be the base of your color palette.")
+
