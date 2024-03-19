5a14acafcea4303c575d7ea980db9e535c556c26
maxine-scott2025 Scott <flojo734@gmail.com>
2024-03-19 21:30:37 +0000

    Finished Function & Project

--- CreateTask.py
+++ CreateTask.py
@@ -1,15 +1,14 @@
-compliment = {"Red": "Green", "Blue": "Orange", "Yellow": "Violet"}
+compliment = {"red": "Green", "blue": "Orange", "yellow": "Violet"}
 
 trio = {"Orange": ["red-orange", "orange", "yellow-orange"], "Green": ["yellow-green", "green", "blue-green"], "Violet": ["blue-violet", "violet", "red-violet"]}
 
-base_color = input("Please input a color to be the base of your color palette or Quit.")
 
 def create(compliment):
+  base_color = input("Please input a color to be the base of your color palette or Quit.")
   while base_color != "Quit":
     
-    if base_color == compliment.keys():
+    if base_color in compliment.keys():
 
-       print(trio[compliment[base_color]])
-create(compliment)  
-
-
+      print(trio[compliment[base_color]])
+    base_color = input("If you are satisfied with this color palette please enter Quit otherwise enter continue.")
+print(create(compliment))
