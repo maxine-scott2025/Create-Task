7150e7c4a69aac556b7a42aaaa79c61ced51ee32
maxine-scott2025 Scott <flojo734@gmail.com>
2024-03-19 05:32:20 +0000

    added function but not working

--- CreateTask.py
+++ CreateTask.py
@@ -2,5 +2,14 @@ compliment = {"Red": "Green", "Blue": "Orange", "Yellow": "Violet"}
 
 trio = {"Orange": ["red-orange", "orange", "yellow-orange"], "Green": ["yellow-green", "green", "blue-green"], "Violet": ["blue-violet", "violet", "red-violet"]}
 
-base_color = input("Please input a color to be the base of your color palette.")
+base_color = input("Please input a color to be the base of your color palette or Quit.")
+
+def create(compliment):
+  while base_color != "Quit":
+    
+    if base_color == compliment.keys():
+
+       print(trio[compliment[base_color]])
+create(compliment)  
+
 
